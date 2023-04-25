efbcb751939e312a14af2da13c8d5ce72dffced1
ali <ali@DESKTOP-M9KH9RL>
2023-04-26 01:12:25 +0430

    Fixing Row Number Visibility Problem.Add project files.

---
+++ Dto/PersonClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dto
+{
+    
+
+    public class Person
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Surname is required")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Position is required")]
+        public string Position { get; set; }
+
+        [Range(1, 120, ErrorMessage = "Age must be between 1 and 120")]
+        public int Age { get; set; }
+    }
+}
